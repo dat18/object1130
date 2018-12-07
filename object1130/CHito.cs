6547c8f853150f2fb4a7c98bea9cb97e4c41063b
tanakaedu <tanaka@am1.jp>
2018-12-07 13:53:40 +0900

    CHito実装

--- object1130/CHito.cs
+++ object1130/CHito.cs
@@ -8,6 +8,28 @@ namespace object1130
 {
 	class CHito
 	{
+		private int count = 0;
+		private string name = "";
 
+		public CHito(string nm)
+		{
+			name = nm;
+			count++;
+		}
+
+		public int getCount()
+		{
+			return count;
+		}
+
+		public string hello()
+		{
+			return "私は" + name + "です。";
+		}
+
+		public string action()
+		{
+			return "テクテク";
+		}
 	}
 }
